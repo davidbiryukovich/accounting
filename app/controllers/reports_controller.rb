351a2caa736af9657c41bec56aec4a9a2ef49772
David Biryukovich <davidbiryukovich@gmail.com>
2018-05-03 23:53:51 +0300

    Add Filter by Categories

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,10 +5,11 @@ class ReportsController < ApplicationController
   end
 
   def transactions
-    relation = Transaction
+    relation = Transaction.where(category_id: current_user.categories.map(&:id))
 
     relation = relation.where('date >= ?', params[:from_date]) if params[:from_date].present?
     relation = relation.where('date <= ?', params[:to_date]) if params[:to_date].present?
+    relation = relation.where('category_id = ?', params[:category_id]) if params[:category_id].present?
 
     @transactions = relation.all
   end
